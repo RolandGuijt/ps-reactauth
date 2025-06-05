0300bf809694f814f704db5d2e21261971060c00
Roland Guijt <roland.guijt@gmail.com>
2025-06-05 11:32:07 +0200

    Add x-csrf header to usePostHouse

--- 3.OpenIDConnectExternalApi/frontend/hooks/usePostHouse.js
+++ 3.OpenIDConnectExternalApi/frontend/hooks/usePostHouse.js
@@ -7,6 +7,7 @@ const usePostHouse = () => {
           headers: {
             Accept: "application/json",
             "Content-Type": "application/json",
+            "x-csrf": "1",
           },
           body: JSON.stringify(house),
         });
